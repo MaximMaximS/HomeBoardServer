e099cea9ed41dea5c0813cad167cc21b802b8183
MaximMaximS <sklenicka.maxim@gmail.com>
2022-05-14 16:12:50 +0200

    Add rate limiting

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
+const slowdown = require("express-slow-down");
+const ratelimit = require("express-rate-limit");
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +10,21 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  slowdown({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    delayAfter: 50,
+    delayMs: 500,
+  })
+);
+
+app.use(
+  ratelimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
